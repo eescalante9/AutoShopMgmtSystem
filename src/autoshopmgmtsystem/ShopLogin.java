761bd5961c27d2aa25b7e790913ead41bd2791af
eescalante9 <42966523+eescalante9@users.noreply.github.com>
2022-06-18 02:19:51 -0400

    First Commit

---
+++ src/autoshopmgmtsystem/ShopLogin.java
@@ -0,0 +1,146 @@
+package autoshopmgmtsystem;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.sql.*;
+import java.awt.event.ActionEvent;
+import javax.swing.JPasswordField;
+
+public class ShopLogin extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textFieldUsername;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ShopLogin frame = new ShopLogin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ShopLogin() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JLabel lblTitle = new JLabel("Mechanic Shop");
+		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitle.setFont(new Font("Arial", Font.BOLD, 22));
+		
+		textFieldUsername = new JTextField();
+		textFieldUsername.setColumns(10);
+		
+		JLabel lblUsername = new JLabel("Username:");
+		
+		JLabel lblPassword = new JLabel("Password:");
+		
+		JButton btnSubmit = new JButton("Submit");
+		btnSubmit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnSubmitAction(e);
+			}
+		});
+		
+		passwordField = new JPasswordField();
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addContainerGap()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addComponent(lblUsername)
+										.addComponent(lblPassword))
+									.addGap(18)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+										.addComponent(passwordField)
+										.addComponent(textFieldUsername, GroupLayout.DEFAULT_SIZE, 163, Short.MAX_VALUE))
+									.addGap(73))
+								.addComponent(lblTitle, GroupLayout.DEFAULT_SIZE, 404, Short.MAX_VALUE)))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(180)
+							.addComponent(btnSubmit)))
+					.addContainerGap())
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(35)
+					.addComponent(lblTitle)
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textFieldUsername, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblUsername))
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblPassword))
+					.addGap(18)
+					.addComponent(btnSubmit)
+					.addContainerGap(73, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
+	private void btnSubmitAction(ActionEvent e) {
+		String user = textFieldUsername.getText();
+		String pass = passwordField.getText();
+		
+		 try{ Class.forName("com.mysql.jdbc.Driver");
+	        Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/mechanic_shop","root","Peruano93!!");
+	        Statement st=conn.createStatement();
+	        String sql="select * from user_login";
+
+	        ResultSet rs= st.executeQuery(sql);
+	           while(rs.next()){
+	               String username=rs.getString("username");
+	               String password=rs.getString("password");
+	               
+	               if(user.equals(username) && pass.equals(password)){
+	               new MainPage().setVisible(true);
+	               dispose();
+	               }
+	               else{
+	               JOptionPane.showMessageDialog(this, "Incorrect username or password.");
+	               }
+	            }
+		 }
+		 catch(Exception ex){
+			 JOptionPane.showMessageDialog(null, "Connection error.");
+		 }
+		
+		
+	}
+}
