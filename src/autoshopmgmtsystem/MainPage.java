e89305bbafb50d8635a9c0fd29cf8e9c74129567
eescalante9 <42966523+eescalante9@users.noreply.github.com>
2022-06-20 23:51:39 -0400

    Create MainPage.java

---
+++ src/autoshopmgmtsystem/MainPage.java
@@ -0,0 +1,129 @@
+package autoshopmgmtsystem;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.LayoutStyle.ComponentPlacement;
+
+public class MainPage extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainPage frame = new MainPage();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainPage() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JLabel lblNewLabel = new JLabel("Mechanic Shop Home");
+		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 22));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		JButton btnMechanics = new JButton("Mechanics");
+		btnMechanics.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnMechanicsActionPerformed(e);
+			}
+		});
+		
+		JButton btnVehicles = new JButton("Vehicles");
+		btnVehicles.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnVehiclesActionPerformed(e);
+			}
+		});
+		
+		JButton btnLogout = new JButton("Logout");
+		btnLogout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnLogoutActionPerformed(e);
+			}
+		});
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(lblNewLabel, GroupLayout.DEFAULT_SIZE, 404, Short.MAX_VALUE)
+							.addContainerGap())
+						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+							.addComponent(btnMechanics, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
+							.addGap(29)
+							.addComponent(btnVehicles, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
+							.addGap(98))))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(180)
+					.addComponent(btnLogout)
+					.addContainerGap(179, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(33)
+					.addComponent(lblNewLabel)
+					.addGap(44)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnMechanics, GroupLayout.PREFERRED_SIZE, 50, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnVehicles, GroupLayout.PREFERRED_SIZE, 50, GroupLayout.PREFERRED_SIZE))
+					.addGap(37)
+					.addComponent(btnLogout)
+					.addContainerGap(38, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
+	
+	private void btnMechanicsActionPerformed(ActionEvent e) {
+		MechanicsPage obj = new MechanicsPage();
+		obj.setVisible(true);
+		dispose();
+	}
+	
+	private void btnVehiclesActionPerformed(ActionEvent e) {
+		VehiclesPage obj = new VehiclesPage();
+		obj.setVisible(true);
+		dispose();
+		
+	}
+	
+	private void btnLogoutActionPerformed(ActionEvent e) {
+		ShopLogin obj = new ShopLogin();
+		obj.setVisible(true);
+		dispose();
+		
+	}
+	
+
+}
