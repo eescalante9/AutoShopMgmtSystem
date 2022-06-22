932e81b610f6943da576e55b28b5fab4ceacd112
eescalante9 <42966523+eescalante9@users.noreply.github.com>
2022-06-21 20:22:26 -0400

    Create VehiclesPage.java

---
+++ src/autoshopmgmtsystem/VehiclesPage.java
@@ -0,0 +1,210 @@
+package autoshopmgmtsystem;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextField;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+public class RemoveVehicle extends JFrame {
+
+	private JPanel contentPane;
+	private JTable tableRecords;
+	private JTextField textFieldRemoveID;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RemoveVehicle frame = new RemoveVehicle();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public RemoveVehicle() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 600, 450);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JLabel lblNewLabel = new JLabel("Remove Vehicle");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 22));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		
+		JButton btnViewRecords = new JButton("VIEW RECORDS");
+		btnViewRecords.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnViewRecordsActionPerformed(e);
+			}
+		});
+		
+		JLabel lblNewLabel_1 = new JLabel("Enter vehicle ID to remove vehicle:");
+		
+		textFieldRemoveID = new JTextField();
+		textFieldRemoveID.setColumns(10);
+		
+		JButton btnRemove = new JButton("REMOVE");
+		btnRemove.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnRemoveActionPerformed(e);
+			}
+		});
+		
+		JButton btnBack = new JButton("BACK");
+		btnBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnBackActionPerformed(e);
+			}
+		});
+		
+		JButton btnLogout = new JButton("LOGOUT");
+		btnLogout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnLogoutActionPerformed(e);
+			}
+		});
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(lblNewLabel, GroupLayout.DEFAULT_SIZE, 554, Short.MAX_VALUE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(64)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(btnViewRecords)
+								.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 442, GroupLayout.PREFERRED_SIZE)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(lblNewLabel_1)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(textFieldRemoveID, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(btnRemove))))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(btnBack)
+							.addPreferredGap(ComponentPlacement.RELATED, 406, Short.MAX_VALUE)
+							.addComponent(btnLogout)))
+					.addContainerGap())
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblNewLabel)
+					.addGap(29)
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 182, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnViewRecords)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNewLabel_1)
+						.addComponent(textFieldRemoveID, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnRemove))
+					.addPreferredGap(ComponentPlacement.RELATED, 56, Short.MAX_VALUE)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnBack)
+						.addComponent(btnLogout))
+					.addContainerGap())
+		);
+		
+		tableRecords = new JTable();
+		tableRecords.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+			},
+			new String[] {
+				"ID", "Vin", "Owner", "Make", "Model", "Problem"
+			}
+		));
+		tableRecords.getColumnModel().getColumn(5).setPreferredWidth(200);
+		scrollPane.setViewportView(tableRecords);
+		contentPane.setLayout(gl_contentPane);
+	}
+	
+	private void btnViewRecordsActionPerformed(ActionEvent e) {
+		try{ Class.forName("com.mysql.jdbc.Driver");
+		 	Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/mechanic_shop","root","Peruano93!!");
+		 	String sql="select * from vehicles";
+        	PreparedStatement pstmt=conn.prepareStatement(sql);
+        	ResultSet rs= pstmt.executeQuery();
+        	DefaultTableModel tm=(DefaultTableModel)tableRecords.getModel();
+        	tm.setRowCount(0);
+        	while(rs.next()){
+        		Object o[]={rs.getInt("VEHICLE_ID"),rs.getString("VEHICLE_VIN"),rs.getString("VEHICLE_OWNER"),rs.getString("VEHICLE_MAKE"),rs.getString("VEHICLE_MODEL"),rs.getString("VEHICLE_PROBLEM")};
+        		tm.addRow(o);
+        	}
+        	conn.close();
+		}catch(Exception ex){
+			JOptionPane.showMessageDialog(null,ex);
+		}
+	}
+	
+	private void btnRemoveActionPerformed(ActionEvent e) {
+		String vehicleID = textFieldRemoveID.getText();
+		
+		try{Class.forName("com.mysql.jdbc.Driver");
+	 		Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/mechanic_shop","root","Peruano93!!");
+	 		String sql="DELETE from vehicles WHERE VEHICLE_ID = " + vehicleID;
+	 		PreparedStatement pstmt = conn.prepareStatement(sql);
+	 		pstmt.executeUpdate();
+	 		JOptionPane.showMessageDialog(null, "Vehicle has been removed succesfully.");
+	 		conn.close();
+	 		textFieldRemoveID.setText("");
+		}catch(Exception ex){
+			JOptionPane.showMessageDialog(null,ex);
+		}
+	}
+	
+	private void btnBackActionPerformed(ActionEvent e) {
+		VehiclesPage obj = new VehiclesPage();
+		obj.setVisible(true);
+		dispose();
+	}
+	
+	private void btnLogoutActionPerformed(ActionEvent e) {
+		ShopLogin obj = new ShopLogin();
+		obj.setVisible(true);
+		dispose();
+	}
+	
+	
+
+}
